d3874f6cd18fc2ce2d898edd018955da0c9d2e67
J.M. Vishal Rashmika Perera <87840726+VishalRashmika@users.noreply.github.com>
2022-05-11 21:25:14 +0530

    content

---
+++ Michael-Bay-Madlib/README.md
@@ -0,0 +1,2 @@
+# Michael Bay Python Madlib
+ This is a funny project that i did as a beginner when learning python.
---
+++ Michael-Bay-Madlib/michael bay madlib.py
@@ -0,0 +1,128 @@
+def main1():
+    # inputs
+    noun = input("Enter a noun: ")
+    body_part = input("Enter a name of a body part: ")
+    noun1 = input("Enter a noun: ")
+    verb3 = input("Enter a verb: ")
+    noun2 = input("Enter a noun: ")
+    emotion = input("Enter a emotion: ")
+    noun3 = input("Enter a noun: ")
+    verb1 = input("Enter a verb: ")
+    occupation = input("Enter a occupation: ")
+    noun4 = input("Enter a noun: ")
+    shape = input("Enter a shape: ")
+    woman_name = input("Enter a name of a woman: ")
+    verb5 = input("Enter a verb: ")
+    adjective1 = input("Enter a adjective: ")
+    noun6 = input("Enter a noun: ")
+    man_name = input("Enter a name of a man/friend/neighbour etc...: ")
+    noun7 = input("Enter a noun: ")
+    historic_monument = input("Enter a noun: ")
+    noun8 = input("Enter a noun: ")
+    restaurant_name = input("Enter a name of a restaurant: ")
+    noun9 = input("Enter a noun: ")
+    adjective = input("Enter a adjective: ")
+    noun0 = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    verb2 = input("Enter a verb: ")
+    man_name1 = input("Enter  the man's name you entered before: ")
+    verb4 = input("Enter a verb (ing form verb): ")
+    noun5 = input("Enter a noun: ")
+
+
+    print()
+    print()
+    print("                  !!!!!!!!!   michael bay   !!!!!!!!!                         ")
+    print("                                mad lib                                                       ")
+    print("")
+    print("")
+    print(noun + " explodes,causing a " + noun1 + " to blow up and nearby")
+    print("he's being chased by the government,who's trying to " + verb + " him.while")
+    print("on the run,he teams up with an incredibly attractive woman named")
+    print(woman_name + ",Who has an incredible " + body_part + ".She may be ")
+    print("from streets,but she can " + verb1 + " like nobody's business.The duo")
+    print("decide to turn the tables on their pursuers by blowing up a " + noun3 + ",Which")
+    print("triggers a chain reaction,causing the local " + noun4 + "," + restaurant_name + ",and")
+    print("the " + historic_monument + " to explode.Then the bad guys' helicopter gets")
+    print(verb2 + " by a piece of " + noun5 + " from when the " + noun6 + " exploded,")
+    print("and the helicopter explodes and falls onto a " + noun7 + ",causing it to " + verb3 + ",")
+    print("Which shoots a fireball straight into the heart of " + noun8 + " and destroys the ")
+    print("bad guy leader.Everything is a " + adjective + " and the two decide that such a")
+    print(adjective1 + " ordeal has caused them to fall in " + emotion + " with each other.")
+    print("They decide to celebrate by " + verb4 + " on the " + noun9 + ",and they even")
+    print("managed to use a " + noun0 + "from the beginning of the movie,to " + verb5)
+    print("the whole story together")
+
+que1 = input("are yo ready to play the game (yes/no): ")
+
+if que1 == "yes":
+    main1()
+else:
+    print("you can't play this mad lib,because you said no!")
+print()
+
+
+input()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
